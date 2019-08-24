c9a7daa970dc6a9db8aee1188837a4dcd19c7b6d
ridickle7 <ridickle7@gmail.com>
2019-08-24 21:54:13 +0900

    1. Login (only View)

--- app/src/main/java/hackathon/co/kr/ui/login/view/LoginActivity.kt
+++ app/src/main/java/hackathon/co/kr/ui/login/view/LoginActivity.kt
@@ -1,8 +1,11 @@
 package hackathon.co.kr.ui.login.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import hackathon.co.kr.neopen.R
 import hackathon.co.kr.neopen.databinding.ActivityLoginBinding
@@ -24,5 +27,18 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         onDataBinding()
+        subscribeUI()
     }
+
+    fun subscribeUI() {
+        loginVM.toastMessage.observe(this, Observer { message ->
+            when (message) {
+                "회원가입 진행" -> {
+                    startActivity(Intent(this@LoginActivity, AssignActivity::class.java))
+                }
+            }
+            Toast.makeText(this@LoginActivity, message, Toast.LENGTH_SHORT).show()
+        })
+    }
+
 }
